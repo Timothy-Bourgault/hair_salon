2962980b4793072208fb8d3fe38ea5ccb19ef65f
Tim <tim.bourgault@gmail.com>
2016-09-27 18:39:13 -0700

    write a 3rd test and the test will pass because there already exists a getAll method.

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -25,7 +25,7 @@
         {
             // Arrange
             $name = "Betty";
-            $test_Stylist = new Stylist($name, "Wednesday, Friday", "cut, perm, style, shampoo", 1);
+            $test_Stylist = new Stylist($name, "Wednesday, Friday", "cut, perm, style, shampoo");
             // Act
             $result = $test_Stylist->getName();
             // Assert
@@ -42,6 +42,21 @@
             $result = Stylist::getAll();
             // Assert
             $this->assertEquals($test_Stylist, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            // Arrange
+            $name = "Betty";
+            $test_Stylist1 = new Stylist($name, "Wednesday, Friday", "cut, perm, style, shampoo");
+            $test_Stylist1->save();
+            $name = "Marco";
+            $test_Stylist2 = new Stylist($name, "Thursday, Monday", "color, shampoo, style");
+            $test_Stylist2->save();
+            // Act
+            $result = Stylist::getAll();
+            // Assert
+            $this->assertEquals([$test_Stylist1, $test_Stylist2], $result);
         }
     }
  ?>
